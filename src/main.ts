7f111c8bac5105fa872d6f11d65f425daa8d04f3
cunhafellipe <fellipesac@gmail.com>
2026-09-17 22:47:44 -0300

    feat(game): make M2 restaurant editor the default scene

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import { BootScene } from './game/scenes/BootScene';
+import { RestaurantEditorScene } from './game/scenes/RestaurantEditorScene';
 
 /**
  * Application entrypoint.
@@ -18,7 +19,7 @@ const game = new Phaser.Game({
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-  scene: [BootScene],
+  scene: [RestaurantEditorScene, BootScene],
 });
 
 export default game;
