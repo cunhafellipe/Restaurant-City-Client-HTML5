86f94f6e7cac909ac617a653a3fcc24b01a0e429
cunhafellipe <fellipesac@gmail.com>
2026-09-18 10:08:36 -0300

    test(authority): add recovered rotation count to fixture

--- server/src/persistence.rs
+++ server/src/persistence.rs
@@ -193,6 +193,7 @@ mod tests {
                 size_x: 2,
                 size_y: 1,
             },
+            rotation_count: 4,
             flags: PlacementFlags::default(),
         }])
         .unwrap()
